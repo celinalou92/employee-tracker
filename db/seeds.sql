501d0437b4b31659f5f1fbf5807a0bca5bf1736d
Celina Louissaint <celinalouissaint@gmail.com>
2021-07-29 17:39:01 -0700

    updated seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,24 +1,25 @@
 INSERT INTO department (department_name)
 VALUES 
-    ('Sales'),
-    ('Operations'),
-    ('Engineering'),
-    ('HR');
+    1('Sales'),
+    2('Operations'),
+    3('Engineering'),
+    4('HR');
 
 INSERT INTO employee_role (title, salary, department_id)
 VALUES 
-    ('Business Development', 150000, 1),
-    ('Operations Manager', 10000, 2),
-    ('Engineer', 20000, 3),
-    ('HR Manager', 10000, 4),
-    ('Sales Director', 300000, 1);
+    1('Business Development', 150000, 1),
+    2('Operations Manager', 10000, 2),
+    3('Engineer', 20000, 3),
+    4('HR Manager', 10000, 4),
+    5('Sales Director', 300000, 1),
+    6('Sales', 130000, 1)
 
 
--- missing manager id and I don't know what to do with that in the schema
+
 INSERT INTO employee (first_name, last_name, employee_role_id, manager_id)
 VALUES 
-    ('James', 'Butler', 5, NULL),
-    ('Alex', 'Orr', 1, 1),
-    ('Greg', 'Diamond', 2, NULL),
-    ('Alfred', 'Gallegos', 3, NULL),
-    ('Lisa', 'Henderson', 4, NULL);
+    1('James', 'Butler', 1, 1),
+    2('Alex', 'Orr', 6, 1),
+    3('Lisa', 'Henderson', 4, 3),
+    4('Greg', 'Diamond', 2, 3),
+    5('Alfred', 'Gallegos', 3, 5);
